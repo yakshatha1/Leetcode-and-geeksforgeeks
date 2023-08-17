2d9404a6c795f094858736fa60a244f24fe4e43e
Yakshatha <141942355+yakshatha1@users.noreply.github.com>
2023-08-17 15:45:03 +0530

    Added solution - LeetHub

---
+++ Intersection of two arrays - GFG/intersection-of-two-arrays.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+    public static void main(String[] args) {
+
+        // Taking input using class Scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Taking count of total testcases
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n, m;
+
+            // taking count of elements in array a
+            n = sc.nextInt();
+
+            // taking count of elements in array b
+            m = sc.nextInt();
+
+            // Creating 2 arrays of n and m
+            int a[] = new int[n];
+            int b[] = new int[m];
+
+            // inserting elements to array a
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            // inserting elements to array b
+            for (int i = 0; i < m; i++) {
+                b[i] = sc.nextInt();
+            }
+            Solution ob = new Solution();
+            // calling NumberofElementsInIntersection method
+            // and printing the result
+            System.out.println(ob.NumberofElementsInIntersection(a, b, n, m));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Function to return the count of the number of elements in
+    // the intersection of two arrays.
+    public static int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        
+        // Add all elements from array a[] into the HashSet
+        for (int i = 0; i < n; i++) {
+            set.add(a[i]);
+        }
+        
+        int count = 0;
+        
+        // Check each element in array b[] if it exists in the HashSet
+        for (int i = 0; i < m; i++) {
+            if (set.contains(b[i])) {
+                count++;
+                set.remove(b[i]); // Remove the element from the set to avoid counting duplicates
+            }
+        }
+        
+        return count;
+    }
+};
