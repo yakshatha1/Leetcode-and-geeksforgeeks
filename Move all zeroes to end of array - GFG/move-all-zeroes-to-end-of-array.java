6a77bf4e94f6442f61e6da6b17f41952bb74d920
Yakshatha <141942355+yakshatha1@users.noreply.github.com>
2023-08-21 13:59:51 +0530

    Added solution - LeetHub

---
+++ Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        while (tc-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n];
+            String[] inputLine = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().pushZerosToEnd(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution {
+    void pushZerosToEnd(int[] arr, int n) {
+  
+  if(n==1 || n==0)
+  {
+      return ;
+  }
+  int nz=0;
+  int z=0;
+  while(nz<n){
+      if(arr[nz]!=0)
+      {
+          int temp=0;
+          temp=arr[nz];
+          arr[nz]=arr[z];
+          arr[z]=temp;
+          nz++;
+          z++;
+      }
+      else
+      {nz++;}
+  }}
+  }
+  
+       
+       
+	 
+	 
